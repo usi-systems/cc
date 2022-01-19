8dcc257293b13bb75bbb90858799148e17b37106
Ali Fattaholmanan <alii.fattah@gmail.com>
2022-01-19 17:09:44 +0100

    remove ns3 build directory

--- ns-allinone-3.35/ns-3.35/build/build-status.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python3
-
-# Programs that are runnable.
-ns3_runnable_programs = ['build/scratch/ns3.35-scratch-simulator-optimized', 'build/scratch/ns3.35-timely-leafspine-optimized', 'build/scratch/ns3.35-tcp-leafspine-optimized', 'build/scratch/subdir/ns3.35-subdir-optimized']
-
-# Scripts that are runnable.
-ns3_runnable_scripts = []
-
